883eea526015053705d1304de125529ea5157640
Martin Heidegger <martin.heidegger@gmail.com>
2014-02-08 10:58:51 +0900

    First 11 steps

---
+++ 01/app.js
@@ -0,0 +1,18 @@
+/**
+ * # 普通のNode.js　スクリプト
+ *
+ * スタートの前にsay-moreのツールをインストールのが必要です。
+ *
+ * ```
+ *   npm install say-more -g
+ * ```
+ *
+ * Macのターミナルで
+ *   
+ * ```
+ *   node app.js
+ * ```
+ * 
+ * を呼ぶと何かが出っている。（スピーカー忘れないでください）
+ */
+require("child_process").exec("say-more -f ../a");　
